c4e3afbd1219f8f581421423aa5ee034e244fa9b
George Livas <giorgos.livas.99@gmail.com>
2017-01-21 16:03:27 +0200

    error

--- src/com/glivas/passGen/UserInt.java
+++ src/com/glivas/passGen/UserInt.java
@@ -3,17 +3,27 @@ package com.glivas.passGen;
 public class UserInt {
 
     public static void loadTUI (int numOfCodes) {
-        int numOfCols;
-        int divNumOfPasswords;
+        PrintLogo.codeGen(1);
+
+        int numOfCols = 1;
+        int divNumOfPasswords = 1;
 
         if (numOfCodes < 50) {
             numOfCols = 1;
             divNumOfPasswords = numOfCodes;
         } else if (numOfCodes >= 50 && numOfCodes < 100) {
             numOfCols = 2;
-            divNumOfPasswords =numOfCodes/2;
+            divNumOfPasswords = numOfCodes/2;
         }
 
+        if (numOfCols == 2) {
+            for (int i = 0; i < divNumOfPasswords; i++) {
+                System.out.println(
+                        "_____________________\n" +
+                        "|" +  PassGen.generateCode(1) + {"|"
+                );
+            }
+        }
 
 
 
